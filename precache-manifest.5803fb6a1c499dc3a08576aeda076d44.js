cc6fb97a8bdff298b5c0756a8e99fc0af6b92632
ssrn <pe6i.is4o@gmail.com>
2019-04-16 14:50:07 +0300

    Updates

---
+++ precache-manifest.5803fb6a1c499dc3a08576aeda076d44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5887bed92c78b9ad4f35",
+    "url": "/stopwatch/static/css/main.44a2da11.chunk.css"
+  },
+  {
+    "revision": "5887bed92c78b9ad4f35",
+    "url": "/stopwatch/static/js/main.3e33cd92.chunk.js"
+  },
+  {
+    "revision": "24a3b63eeedaf9f526ca",
+    "url": "/stopwatch/static/js/runtime~main.000e15a5.js"
+  },
+  {
+    "revision": "385b97ab9631856ecf1e",
+    "url": "/stopwatch/static/js/2.330faaeb.chunk.js"
+  },
+  {
+    "revision": "eb5f0947e824d45c44b3967dd24afe93",
+    "url": "/stopwatch/static/media/start.eb5f0947.svg"
+  },
+  {
+    "revision": "2859155ca46b5af0f50b8b2a3211d1e1",
+    "url": "/stopwatch/static/media/pause.2859155c.svg"
+  },
+  {
+    "revision": "05b0922db968c13492baf683f6a836d1",
+    "url": "/stopwatch/static/media/stop.05b0922d.svg"
+  },
+  {
+    "revision": "9f1d153bfbee39524ba1f01c09ec3e51",
+    "url": "/stopwatch/index.html"
+  }
+];
